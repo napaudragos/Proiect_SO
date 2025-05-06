2afa65424064261203997bf0c9dce89d90604d42
napaudragos <dragos.napau@gmail.com>
2025-05-06 22:20:08 +0300

    main

--- monitor.c
+++ monitor.c
@@ -3,23 +3,103 @@
 #include <string.h>
 #include <windows.h>
 
+// Handler pentru "semnalul" de oprire
 void handle_SIGTERM()
 {
     printf("Shutting down after delay...\n");
     fflush(stdout);
-    // Delay de 3 secunde
-    Sleep(3000); // Sleep acceptă timpul în milisecunde pe Windows
+    Sleep(3000); // Pauză de 3 secunde
     printf("Terminated.\n");
-    exit(0); // Termină procesul
+    exit(0);
 }
 
-int main()
+// Handler pentru comenzi din prm.txt
+void handle_monitor_command()
 {
-    // Simulăm primirea unui semnal SIGTERM
-    printf("Press Enter to simulate SIGTERM...\n");
-    getchar(); // Așteptăm ca utilizatorul să apese Enter
+    FILE *f = fopen("prm.txt", "r");
+    if (!f)
+    {
+        printf("Could not open prm.txt\n");
+        return;
+    }
 
-    handle_SIGTERM();
+    char args[3][100];
+    int count = 0;
+
+    while (fgets(args[count], sizeof(args[count]), f) && count < 3)
+    {
+        args[count][strcspn(args[count], "\n")] = '\0'; // Scoatem newline
+        count++;
+    }
+    fclose(f);
+
+    remove("prm.txt");
+
+    if (count == 0)
+    {
+        printf("prm.txt is empty\n");
+        return;
+    }
+
+    // Construim linia de comandă
+    char commandLine[256] = "treasure_manager";
+    for (int i = 0; i < count; ++i)
+    {
+        strcat(commandLine, " ");
+        strcat(commandLine, args[i]);
+    }
+
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+
+    if (!CreateProcess(
+            NULL,          // Numele aplicației (NULL pentru a folosi commandLine)
+            commandLine,   // Comandă cu parametri
+            NULL, NULL, FALSE,
+            0, NULL, NULL,
+            &si, &pi))
+    {
+        printf("CreateProcess failed (%lu).\n", GetLastError());
+        return;
+    }
+
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+}
+
+// Verifică dacă fișierul există
+int file_exists(const char *filename)
+{
+    DWORD attrib = GetFileAttributesA(filename);
+    return (attrib != INVALID_FILE_ATTRIBUTES &&
+            !(attrib & FILE_ATTRIBUTE_DIRECTORY));
+}
+
+// Funcția principală
+int main(void)
+{
+    printf("Monitor started. Waiting for commands...\n");
+
+    while (1)
+    {
+        if (file_exists("prm.txt"))
+        {
+            handle_monitor_command();
+        }
+
+        if (file_exists("stop.txt"))
+        {
+            // După procesare, poți și șterge stop.txt dacă vrei
+            remove("stop.txt");
+            handle_SIGTERM();
+        }
+
+        Sleep(1000); // Așteaptă 1 secundă între verificări
+    }
 
     return 0;
 }
