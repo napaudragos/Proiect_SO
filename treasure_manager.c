ea20a5d67a84469a2ee29c70ef813bd020983dc1
napaudragos <dragos.napau@gmail.com>
2025-05-18 10:33:10 +0000

    Update treasure_manager

--- treasure_manager.c
+++ treasure_manager.c
@@ -22,68 +22,9 @@ typedef struct {
     int value;
 } Treasure;
 
-// Function declarations
-void add_treasure(const char *hunt_id);
-void list_treasures(const char *hunt_id);
-void view_treasure(const char *hunt_id, int treasure_id);
-void remove_treasure(const char *hunt_id, int treasure_id);
-void remove_hunt(const char *hunt_id);
-void log_operation(const char *hunt_id, const char *message);
-void create_symlink(const char *hunt_id);
-void list_hunts();
 
-int main(int argc, char **argv)
-{
-    if (argc < 2 || (strcmp(argv[1], "--list_hunts") != 0 && argc < 3))
-    {
-        fprintf(stderr, "Usage:\n"
-                        "--add <hunt_id>\n"
-                        "--list <hunt_id>\n"
-                        "--view <hunt_id> <treasure_id>\n"
-                        "--remove_treasure <hunt_id> <treasure_id>\n"
-                        "--remove_hunt <hunt_id>\n"
-                        "--list_hunts\n");
-        exit(EXIT_FAILURE);
-    }
-    if (strcmp(argv[1], "--add") == 0)
-    {
-        add_treasure(argv[2]);
-    }
-    else if (strcmp(argv[1], "--list") == 0)
-    {
-        list_treasures(argv[2]);
-    }
-    else if (strcmp(argv[1], "--view") == 0)
-    {
-        if (argc < 4) {
-            fprintf(stderr, "Missing treasure_id for --view\n");
-            exit(EXIT_FAILURE);
-        }
-        view_treasure(argv[2], atoi(argv[3]));
-    }
-    else if (strcmp(argv[1], "--remove_treasure") == 0)
-    {
-        if (argc < 4) {
-            fprintf(stderr, "Missing treasure_id for --remove_treasure\n");
-            exit(EXIT_FAILURE);
-        }
-        remove_treasure(argv[2], atoi(argv[3]));
-    }
-    else if (strcmp(argv[1], "--remove_hunt") == 0)
-    {
-        remove_hunt(argv[2]);
-    }
-    else if (strcmp(argv[1], "--list_hunts") == 0)
-    {
-        list_hunts();
-    }
-    else
-    {
-        fprintf(stderr, "Invalid command\n");
-        exit(EXIT_FAILURE);
-    }
-    return 0;
-}
+
+
 
 void log_operation(const char *hunt_id, const char *message) {
     char log_path[MAX_PATHH];
@@ -367,3 +308,55 @@ void list_hunts()
     closedir(dir);
 }
 
+int main(int argc, char **argv)
+{
+    if (argc < 2 || (strcmp(argv[1], "--list_hunts") != 0 && argc < 3))
+    {
+        fprintf(stderr, "Usage:\n"
+                        "--add <hunt_id>\n"
+                        "--list <hunt_id>\n"
+                        "--view <hunt_id> <treasure_id>\n"
+                        "--remove_treasure <hunt_id> <treasure_id>\n"
+                        "--remove_hunt <hunt_id>\n"
+                        "--list_hunts\n");
+        exit(EXIT_FAILURE);
+    }
+    if (strcmp(argv[1], "--add") == 0)
+    {
+        add_treasure(argv[2]);
+    }
+    else if (strcmp(argv[1], "--list") == 0)
+    {
+        list_treasures(argv[2]);
+    }
+    else if (strcmp(argv[1], "--view") == 0)
+    {
+        if (argc < 4) {
+            fprintf(stderr, "Missing treasure_id for --view\n");
+            exit(EXIT_FAILURE);
+        }
+        view_treasure(argv[2], atoi(argv[3]));
+    }
+    else if (strcmp(argv[1], "--remove_treasure") == 0)
+    {
+        if (argc < 4) {
+            fprintf(stderr, "Missing treasure_id for --remove_treasure\n");
+            exit(EXIT_FAILURE);
+        }
+        remove_treasure(argv[2], atoi(argv[3]));
+    }
+    else if (strcmp(argv[1], "--remove_hunt") == 0)
+    {
+        remove_hunt(argv[2]);
+    }
+    else if (strcmp(argv[1], "--list_hunts") == 0)
+    {
+        list_hunts();
+    }
+    else
+    {
+        fprintf(stderr, "Invalid command\n");
+        exit(EXIT_FAILURE);
+    }
+    return 0;
+}
